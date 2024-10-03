211f18d976befbc4fed55a2fbb3eab92d68268bd
cmsolson75 <78053882+cmsolson75@users.noreply.github.com>
2024-10-03 15:13:46 -0700

    feat(simpleGo/calculator): got cli interface working.

---
+++ simpleGo/calculator/README.md
@@ -0,0 +1,15 @@
+# Calculator App
+
+## Project Scope
+I want to get more experience with CLI libraries in go, so I am going to take a simple program and focus on the interface.
+
+## Notes
+
+Requirements of interface
+
+./calc add 1 2
+stdout: 3
+
+I am going to keep this simple: there are intresting ways I could use methods and structs, but this is my first attempt, so lets keep it simple.
+
+Maybe just make the user input a struct
--- simpleGo/calculator/calculator.go
+++ simpleGo/calculator/calculator.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func Add(a, b float64) (float64, bool) {
 	return a + b, false
 }
@@ -19,5 +26,55 @@ func Divide(a, b float64) (float64, bool) {
 	} else {
 		result := 0.0
 		return result, true
+	}
+}
+
+func getArgs() (float64, float64) {
+	// check flags
+	if flag.Arg(0) == "" || flag.Arg(1) == "" {
+		fmt.Println("Please provide arguments")
+		os.Exit(0)
+	}
+
+	// parse a
+	a, err := strconv.ParseFloat(flag.Arg(0), 64)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+
+	// parse b
+	b, err := strconv.ParseFloat(flag.Arg(1), 64)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+	return a, b
+}
+
+func main() {
+	useAdd := flag.Bool("add", false, "use add command")
+	useSub := flag.Bool("sub", false, "use subtract command")
+	useMul := flag.Bool("mul", false, "use multiply command")
+	useDiv := flag.Bool("div", false, "use divide command")
+	flag.Parse()
+	a, b := getArgs()
+
+	if *useAdd {
+		output, _ := Add(a, b)
+		fmt.Printf("%.2f + %.2f = %.2f\n", a, b, output)
+	} else if *useSub {
+		output, _ := Subtract(a, b)
+		fmt.Printf("%.2f - %.2f = %.2f\n", a, b, output)
+	} else if *useMul {
+		output, _ := Multiply(a, b)
+		fmt.Printf("%.2f * %.2f = %.2f\n", a, b, output)
+	} else if *useDiv {
+		output, err := Divide(a, b)
+		if err {
+			fmt.Println("Cant divide by 0")
+		} else {
+			fmt.Printf("%.2f / %.2f = %.2f\n", a, b, output)
+		}
 	}
 }
