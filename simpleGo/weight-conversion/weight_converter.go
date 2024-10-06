c8a04abe06a2fe54c9ce60272392f1ed9525d2b8
cmsolson75 <78053882+cmsolson75@users.noreply.github.com>
2024-10-05 17:11:10 -0700

    feat(simpleGo/weight-conversion): got it working for g, oz, lbs, kg with input

--- simpleGo/weight-conversion/weight_converter.go
+++ simpleGo/weight-conversion/weight_converter.go
@@ -33,26 +33,123 @@ func LbsConverter(unit string, weight float64) float64 {
 	case "oz":
 		output := LbsToOz(weight)
 		return output
+	default:
+		return weight
 	}
-	return weight
+}
+
+func OzToLbs(weight float64) float64 {
+	return weight * 0.0625
+}
+
+func OzToG(weight float64) float64 {
+	return weight * 28.34952
+}
+
+func OzToKg(weight float64) float64 {
+	return weight * 0.02834952
+}
+
+func OzConverter(unit string, weight float64) float64 {
+	switch unit {
+	case "lb", "lbs":
+		return OzToLbs(weight)
+	case "g":
+		return OzToG(weight)
+	case "kg":
+		return OzToKg(weight)
+	default:
+		return weight
+	}
+}
+
+func GToKg(weight float64) float64 {
+	return weight / 1000
+}
+
+func GToLbs(weight float64) float64 {
+	return weight * 0.00220462262185
+}
+
+func GToOz(weight float64) float64 {
+	return weight * 0.03527396195
+}
+
+func GConverter(unit string, weight float64) float64 {
+	switch unit {
+	case "lb", "lbs":
+		return GToLbs(weight)
+	case "oz":
+		return GToOz(weight)
+	case "kg":
+		return GToKg(weight)
+	default:
+		return weight
+	}
+}
+
+func KgToG(weight float64) float64 {
+	return weight * 1000
+}
+
+func KgToOz(weight float64) float64 {
+	return weight * 35.27396195
+}
+
+func KgToLbs(weight float64) float64 {
+	return weight * 2.20462262185
+}
+
+func KgConverter(unit string, weight float64) float64 {
+	switch unit {
+	case "lb", "lbs":
+		return KgToLbs(weight)
+	case "oz":
+		return KgToOz(weight)
+	case "g":
+		return KgToG(weight)
+	default:
+		return weight
+	}
 }
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Input a weight (float)")
+	fmt.Println("Input a weight (float) and a unit (lbs, g, kg, oz)")
 	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
-	weight, err := strconv.ParseFloat(input, 64)
+	line := strings.TrimSpace(input)
+	inputs := strings.Fields(line)
+	if len(inputs) < 2 {
+		fmt.Println("Not enough inputs exiting...")
+		os.Exit(1)
+	}
+	weight, err := strconv.ParseFloat(inputs[0], 64)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
+	inputUnit := strings.ToLower(inputs[1])
+
 	fmt.Println("Input a unit to convert")
 	unit, _ := reader.ReadString('\n')
 	unit = strings.ToLower(strings.TrimSpace(unit))
-	newWeight := LbsConverter(unit, weight)
+	var newWeight float64
+
+	switch inputUnit {
+	case "lb", "lbs", "pounds", "pound":
+		newWeight = LbsConverter(unit, weight)
+	case "oz", "ounce":
+		newWeight = OzConverter(unit, weight)
+	case "kg", "kgs", "kilogram", "kilograms":
+		newWeight = KgConverter(unit, weight)
+	case "g", "gs", "gram", "grams":
+		newWeight = GConverter(unit, weight)
+	default:
+		newWeight = weight
+	}
+
 	fmt.Println("New Weight: ", newWeight)
 
 }
--- simpleGo/weight-conversion/weight_converter_test.go
+++ simpleGo/weight-conversion/weight_converter_test.go
@@ -1,10 +1,26 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+func withinTolerance(a, b, e float64) bool {
+	if a == b {
+		return true
+	}
+	d := math.Abs(a - b)
+
+	if b == 0 {
+		return d < e
+	}
+
+	return (d / math.Abs(b)) < e
+}
 
 func checkWeightOutput(t *testing.T, got, want float64) {
-	if got != want {
-		t.Errorf("got %f want %f", got, want)
+	if !withinTolerance(got, want, 1e-10) {
+		t.Errorf("got %.18f want %.18f", got, want)
 	}
 }
 
@@ -29,5 +45,81 @@ func TestLbsToOz(t *testing.T) {
 func TestLbsConverter(t *testing.T) {
 	got := LbsConverter("oz", 10)
 	want := 160.0
+	checkWeightOutput(t, got, want)
+}
+
+func TestOzToLbs(t *testing.T) {
+	got := OzToLbs(15.0)
+	want := 0.9375
+	checkWeightOutput(t, got, want)
+}
+
+func TestOzToG(t *testing.T) {
+	got := OzToG(15.0)
+	want := 425.2428
+	checkWeightOutput(t, got, want)
+}
+
+func TestOzToKg(t *testing.T) {
+	got := OzToKg(15.0)
+	want := 0.4252428
+	checkWeightOutput(t, got, want)
+}
+
+func TestOzConverter(t *testing.T) {
+	got := OzConverter("lb", 15.0)
+	want := 0.9375
+	checkWeightOutput(t, got, want)
+}
+
+// ---- G converter
+func TestGToKg(t *testing.T) {
+	got := GToKg(15.0)
+	want := 0.015
+	checkWeightOutput(t, got, want)
+
+}
+
+func TestGToLbs(t *testing.T) {
+	got := GToLbs(15.0)
+	want := 0.0330693393277
+	checkWeightOutput(t, got, want)
+}
+
+func TestGToOz(t *testing.T) {
+	got := GToOz(15.0)
+	exp := 0.52910942925
+	checkWeightOutput(t, got, exp)
+}
+
+func TestGConverter(t *testing.T) {
+	got := GConverter("oz", 15)
+	want := 0.52910942925
+	checkWeightOutput(t, got, want)
+}
+
+// // ---- Kg converter
+
+func TestKgToG(t *testing.T) {
+	got := KgToG(15.0)
+	want := 15000.0
+	checkWeightOutput(t, got, want)
+}
+
+func TestKgToOz(t *testing.T) {
+	got := KgToOz(15.0)
+	want := 529.10942925
+	checkWeightOutput(t, got, want)
+}
+
+func TestKgToLbs(t *testing.T) {
+	got := KgToLbs(15.0)
+	want := 33.0693393278
+	checkWeightOutput(t, got, want)
+}
+
+func TestKgConverter(t *testing.T) {
+	got := KgConverter("oz", 15)
+	want := 529.10942925
 	checkWeightOutput(t, got, want)
 }
